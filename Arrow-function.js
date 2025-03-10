665926a282164b4233aab4aac0a09a4eb1f9edcd
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-03-10 17:45:37 +0600

    normal-function and arrow-function

---
+++ Arrow-function.js
@@ -0,0 +1,9 @@
+const sum = (num1, num2) => {
+    return num1+num2;
+}
+console.log(sum(40,39));
+
+const avs = (num1) => {
+    console.log("hello",num1)
+}
+avs(30)
---
+++ Normal-function.js
@@ -0,0 +1,9 @@
+function sum (num1, num2){
+    return num1+num2;
+}
+console.log(sum(10,50));
+
+function sum2 (num1, num2, num3, num4){
+    return num1+num2+num3+num4
+}
+console.log(sum2(10,50, 30, 40));
