c1fafb377a79b398bb219d2215025da77b3bd1a6
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-03-11 17:06:10 +0600

    Spreas-operator and Math-Moth

--- Spreas-Operator.js
+++ Spreas-Operator.js
@@ -2,3 +2,8 @@ const frineds = ["Sojib", "Sakib", "Sihab"];
 
 const newfrineds = ["Shajalal", ...frineds];
 console.log(newfrineds);
+
+const number = [2,45,32,789,3,2,56,43,98,23,]
+
+console.log(Math.max(...number));
+console.log(Math.min(...number))
