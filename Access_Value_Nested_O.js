95e9ffe08ee545bbc913dc5320bf23db51ff8dc5
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-03-13 00:48:11 +0600

    Access Value, nested object, Optional chaining etc

--- Access_Value_Nested_O.js
+++ Access_Value_Nested_O.js
@@ -2,6 +2,8 @@ const person = {
     Name : "Shajalal",
     age : 24,
     friends : ["Sakib", "Sakil"],
+    1:"100",
+    true : "sts",
     details: {
         job : "yes",
         isMarrid : false,
@@ -15,6 +17,10 @@ const person = {
         }
     }
 }
+
+console.log(person[true])
+
+console.log(person["details"]["job"])
 
 console.log(person.details.father.Mother.Name)
 console.log(person.details.father.Mother.bather?.Name)
