111ccbff5645c77e23d932d4a0d4921c17b4866e
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-03-13 01:16:28 +0600

    Array map to do one-line loop magic

--- Array_Map.js
+++ Array_Map.js
@@ -3,3 +3,19 @@ const newArray = numbers.map(numbers => {
     return numbers+2
 });
 console.log(newArray);
+
+// SquareArray
+
+const  numb = [1, 2, 3, 4, 5, 6];
+const SquareArray = numb.map(element => {
+    return element * element
+})
+console.log(SquareArray)
+
+
+// friends list 
+const friends = ["rahim", "Korim", "jabbar"]
+const newFriends = friends.map ((element, index) => {
+    console.log(index)
+    console.log(element)
+})
