ca09140ce1fcc86c608356b25e56171cce1acc09
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-07 23:43:18 +0600

    if_else_Statements

---
+++ conditionals_statement/if_else_statements.js
@@ -0,0 +1,17 @@
+let age = 20;
+let hasTicket = true;
+
+if (age >= 18 && hasTicket) {
+    console.log("You can enter the concert.");
+} else {
+    console.log("You cannot enter the concert.");
+}
+ 
+// if ((age >= 19 && hasTicket)|| hasTicket){
+//     console.log("You can enter the concert");
+
+// }
+// else
+// {
+//     console.log("You cannot enter the concert");
+// }
---
+++ conditionals_statement/if_statement.js
@@ -0,0 +1,8 @@
+let age = 18;
+
+if (age >= 19) {
+    console.log("You are an adult.");
+}
+ else {
+    console.log("This is not age ");
+ }
