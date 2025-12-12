fb19da9d80a55ef49a755de7a15944208d46898f
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-13 00:26:57 +0600

    Fundamentals of string objects in js

---
+++ String_Objects/string_objects.js
@@ -0,0 +1,19 @@
+// Create an Object:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue" ,
+  salay: 19000
+};
+
+ const incam = person.salay;
+
+
+// Display Data from the Object:
+console.log(person.age );
+console.log(person.firstName);
+console.log(incam);
+ 
+
+ 
