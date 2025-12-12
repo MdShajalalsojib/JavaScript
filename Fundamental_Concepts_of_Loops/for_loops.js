d603692f799279cc6b180d233f1dd9d1d4bc0047
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-12 22:35:18 +0600

    for loops

--- Fundamental_Concepts_of_Loops/for_loops.js
+++ Fundamental_Concepts_of_Loops/for_loops.js
@@ -6,3 +6,12 @@ for( let i=5 ; i<=20 ; i++){
 }
 
   console.log("The all number sum : ",sum);
+
+
+
+
+  for(let i = 1; i<= 30 ; i++){
+    if (i % 3 === 0 || i % 5 === 0){
+        console.log(i);
+    }
+  }
