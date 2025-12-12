0cfbd3d640e2487f306f497d76a384f6879aad83
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-12 22:22:47 +0600

    while loops sum

--- Fundamental_Concepts_of_Loops/while_loops.js
+++ Fundamental_Concepts_of_Loops/while_loops.js
@@ -1,5 +1,13 @@
- let i = 1;
-while (i <= 5) {
-    console.log(i);
+//  let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+let sum = 0;
+let i = 1;
+while (sum < 20) {
+    sum += i;
     i++;
 }
+console.log("Final Sum =", sum);
