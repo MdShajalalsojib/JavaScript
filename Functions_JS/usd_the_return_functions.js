1e1dccf47faf032d44478e97f0186d952517eb1a
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-13 12:43:36 +0600

    usd the return functions

---
+++ Functions_JS/usd_the_return_functions.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 3);
+console.log(result); 
+
+
+// String return
+
+function greet(name) {
+    return "Hello " + name;
+}
+
+console.log(greet("Sojib"));
+
