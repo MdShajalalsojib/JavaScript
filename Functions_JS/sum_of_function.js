6b3b5ccfcaaf1d553d585bef3dcc5a17b454a3e1
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-13 12:24:25 +0600

    functions usd the sum dib Multip

---
+++ Functions_JS/sum_of_function.js
@@ -0,0 +1,17 @@
+function add(number1, number2){
+    const sum = number1 + number2;
+    console.log(sum);
+}
+
+function num(a , b ){
+    const Dis = a - b ;
+    console.log(Dis);
+}
+
+function Mult(a, b, c){
+    const Multip = a * b * c;
+    console.log(Multip);
+}
+  add(38, 23);
+  num(30 , 20);
+  Mult(2, 5, 3);
