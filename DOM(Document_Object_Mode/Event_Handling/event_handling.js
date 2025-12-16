a2e62ede21a84b6b728396003e092a64eccab62f
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-16 15:25:39 +0600

    Commite post system DOM

--- DOM(Document_Object_Mode/Event_Handling/event_handling.js
+++ DOM(Document_Object_Mode/Event_Handling/event_handling.js
@@ -27,5 +27,52 @@ document.getElementById("btn").addEventListener('dblclick',
 document.getElementById("btn_login").addEventListener('click', function(){
     const BtnLogin = document.getElementById("no_login");
     BtnLogin.innerText = 'User login successfully';
-})
+});
+
+//    set event listener 
+   document.getElementById('btn_up')
+       .addEventListener('click',function(){
+
+           console.log('update button clicked');
+            // get the text from the input field 
+            const nameInput = document.getElementById('input-name');
+            const name = nameInput. value;
+            console.log('name',name);
+
+            // set the name 
+            const nameP = document.getElementById('name');
+            nameP.innerText = name;
+       });
+
+
+    //    step--1: set an event handler to the button 
+         document.getElementById('btn_Commit_Post')
+         .addEventListener('click',function(){
+
+
+            // step--2: get the text written in teh comment text area 
+             const commentTextBox = document.getElementById('Commit_Text');
+
+             const newComment = commentTextBox.value;
+
+            //  step--3: get the parent node where to pubbest commit 
+              
+            const commentContainer = document.getElementById('Commit_box')
+            console.log(commentContainer);
+
+            // step--4: create a comment p and set the this.innerText 
+
+            const commentElement = document.createElement('p');
+            commentElement.classList.add('commit');
+            commentElement.innerText = newComment;
+
+            // step--5: append the new p tag to the parent node 
+
+            commentContainer.appendChild(commentElement);
+
+            // step--6: clean the text area 
+            
+            commentTextBox.value = '';
+
+         })
 
