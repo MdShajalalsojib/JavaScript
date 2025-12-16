2fbe54160eff12b13740a60abd7b53bd6c966583
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-16 13:07:29 +0600

    Event Handling

---
+++ DOM(Document_Object_Mode/Event_Handling/event_handling.js
@@ -0,0 +1,15 @@
+// Option ---------02
+function mekeRead (){
+    document.body.style.backgroundColor = 'red';
+
+};
+function mekeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+// Option-----------03
+
+const btnMake = document.getElementById('btn_make');
+  btnMake.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+  }
+
