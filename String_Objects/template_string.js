a80ba110ed7319a6e72b3a41ec05ab2633eee4d7
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-17 13:17:38 +0600

    Template_String

--- String_Objects/template_string.js
+++ String_Objects/template_string.js
@@ -1,4 +1,10 @@
 const count = "Shajalal";
 
 const anthem = `My name is ${count} I ma student`;
+
+const number = `eijfs eiwuwn
+eirjw sfjee
+seirjwe 
+eriwijf `;
+console.log( number);
 console.log(anthem);
