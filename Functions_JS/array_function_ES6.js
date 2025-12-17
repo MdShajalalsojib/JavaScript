e26582bee67ea055e186740136b1c10bcd38dcb1
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-17 13:46:34 +0600

    ArrowFunction (ES6)

---
+++ Functions_JS/array_function_ES6.js
@@ -0,0 +1,32 @@
+// ES6 Arrow Function
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(5, 3));
+
+// One-line Arrow Function
+
+const add2 = (a, b) => a + b;
+
+console.log(add2(10, 20)); 
+
+// Single Parameter
+
+const square = x => x * x;
+
+console.log(square(5)); 
+
+// Arrow Function with Array
+
+const numbers = [1, 2, 3, 4];
+
+const double = numbers.map(num => num * 2);
+
+console.log(double);  
+
+
+
+
+
+
