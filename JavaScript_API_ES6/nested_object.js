2a9238e69085d517170bbd2799699ee35d5b141f
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-18 14:59:12 +0600

    Nested Object

---
+++ JavaScript_API_ES6/nested_object.js
@@ -0,0 +1,21 @@
+const person = {
+      Name:  "Sojib",
+      age: 23,
+      conter:"BD",
+      student: {
+        StudentName: "Sakib",
+        Stu_age: 25,
+        Dep: "CSE",
+        Student_Sud:["Cse_403","software Engineering", "CSE-315"],
+           Student_Detes: {
+              Stu_fatherName: "siejfe",
+              stu_MatherName:"sierjei",
+                father_Des: {
+                    fei:"eij"
+                }
+           }
+
+      }
+
+}
+console.log(person.student.Student_Detes.father_Des.fei);
