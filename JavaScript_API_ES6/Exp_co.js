0a6df5fccac6c2ac74c05c0eaf48fc8e220ee417
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-21 23:31:16 +0600

    Nothen

---
+++ JavaScript_API_ES6/Exp_co.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Exp_co = () => {
+    return (
+        <div>
+            <p>hello </p>
+            <h1>lorgentlsei eijf</h1>
+        </div>
+    );
+};
+
+export default Exp_co;
