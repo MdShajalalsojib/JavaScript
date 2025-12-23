4e48c251d0cfa955c564ba9f3efd2a12374fc0d8
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-12-23 20:51:21 +0600

    Json

--- JavaScript_API_ES6/Exp_co.js
+++ JavaScript_API_ES6/Exp_co.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Exp_co = () => {
-    return (
-        <div>
-            <p>hello </p>
-            <h1>lorgentlsei eijf</h1>
-        </div>
-    );
+  return (
+    <div>
+      <p>hello </p>
+      <h1>lorgentlsei eijf</h1>
+    </div>
+  );
 };
 
 export default Exp_co;
