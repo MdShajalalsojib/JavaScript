66c400d4f0696c55956d3cdc5bd60d6436878b46
MdShajalalsojib <shahjalalsojib088@gmail.com>
2026-01-15 22:22:30 +0600

    object to json bata comet

---
+++ JSON/json.js
@@ -0,0 +1,10 @@
+const prerson = {
+    name : 'sojib',
+    age : 23,
+    Dep : 'cse',
+    friendes : ["hena","alam"],
+    status:true,
+
+}
+const newPerson = JSON.stringify(prerson);
+console.log(newPerson)
