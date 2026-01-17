2c9d9de3805a4fe7c206f5f13acc33754144de7c
MdShajalalsojib <shahjalalsojib088@gmail.com>
2026-01-17 23:33:08 +0600

    Css Design

--- JSON/JSON Fatects/moreAPI.js
+++ JSON/JSON Fatects/moreAPI.js
@@ -11,9 +11,15 @@ const displayUseer = (users) => {
     const userContainer = document.getElementById('users');
 
     for (const user of users){
-        console.log(user.name)
+        console.log(user)
         const div = document.createElement("div");
-        div.innerText = user.name;
+
+        div.classList.add("card")
+        div.innerHTML = `
+        <h1>${user.name}</h1>
+        <h2>${user.username}</h2>
+        <p>${user.email}</p>
+        `;
         userContainer.appendChild(div);
     }
 }
